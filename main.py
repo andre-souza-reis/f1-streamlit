4fbb4589cd23a88b79d7fb9a8db4dbb7936a2bab
Reis, André de Souza <andre.de.souza.reis@accenture.com>
2022-06-02 16:19:42 -0300

    Circuit page

--- main.py
+++ main.py
@@ -16,8 +16,10 @@ class f1():
         self.navigation = ''
         self.pilot_name = ''
         self.all_pilots = ''
+        self.all_circuits = ''
         self.driverid = ''
         self.driver_details = ''
+        self.circuit_name = ''
 
         self.main_page()
 
@@ -51,7 +53,7 @@ class f1():
 
             self.navigation = st.radio(
                 "Go to:",
-                ("Circuit", "Championship", "Driver", "Race")
+                ("Circuit", "Championship", "Driver", "Race", "About")
             )
 
             if self.navigation == 'Driver':
@@ -70,8 +72,45 @@ class f1():
                 f'Drivers with a name similar to {pilot_name_search}',
                 tuple(response))
 
+            if self.navigation == 'Circuit':
+                #Cache
+                self.all_circuits = query_all_circuits()
+
+                # Search name
+                st.subheader('Search circuit')
+
+                countries = list(self.all_circuits['Country'].unique().astype(str))
+                countries.sort()
+
+                # Search name
+                country = st.selectbox(f'Select circuit country', countries)
+
+                circuits = list(self.all_circuits[self.all_circuits['Country'] == country]['Name'])
+
+                self.circuit_name = st.selectbox(f'Select circuit', circuits)
+
     def circuit_page(self):
         st.header('Circuit')
+
+        circuit_details = query_circuit_details(self.circuit_name, self.all_circuits)
+        
+        st.header(self.circuit_name)
+
+        st.subheader('Map of region')
+
+        lat = float(self.all_circuits[self.all_circuits['Name'] == self.circuit_name]['Geo'].values[0]['lat'])
+        long = float(self.all_circuits[self.all_circuits['Name'] == self.circuit_name]['Geo'].values[0]['long'])
+
+        df = pd.DataFrame([[lat, long]], columns=['lat', 'lon'])
+        st.map(df, zoom=14, use_container_width=True)
+
+        st.subheader(circuit_details[1] + ' Results')
+
+        st.write(circuit_details[2])
+
+        st.subheader('Past Winners')
+
+        st.write(circuit_details[0])
 
     def race_page(self):
         st.header('Race')
@@ -156,6 +195,20 @@ def query_all_pilots():
     pilots = pilots.assign(Name = pilots[['GivenName', 'FamilyName']].apply(' '.join, axis = 1)).drop(['GivenName', 'FamilyName'], axis = 1)
 
     return pilots
+
+def query_all_circuits():
+    data = requests.request("GET", 'http://ergast.com/api/f1/circuits?limit=10000').text
+    tree = ET.fromstring(data)
+
+    circuits = []
+    for i in range(len(tree[0])):
+        circuitId = tree[0][i].attrib['circuitId']
+        circuit = tree[0][i].find('{http://ergast.com/mrd/1.5}CircuitName').text
+        geo = tree[0][i].find('{http://ergast.com/mrd/1.5}Location').attrib
+        country = tree[0][i].find('{http://ergast.com/mrd/1.5}Location').find('{http://ergast.com/mrd/1.5}Country').text
+        circuits.append([circuitId, circuit, geo, country])
+
+    return pd.DataFrame(circuits, columns=['ID', 'Name', 'Geo', 'Country'])
 
 def query_driver(all_pilots, name):
     
@@ -275,5 +328,41 @@ def query_driver_actual_details(driver_id):
     wins = tree[0][0][0].attrib['wins']
 
     return [position, points, wins]
+
+def query_circuit_details(circuit_name, all_circuits):
+    circuit_id = all_circuits[all_circuits['Name'] == circuit_name]['ID'].values[0]
+
+    data = requests.request("GET", f'http://ergast.com/api/f1/circuits/{circuit_id}/results/1?limit=1000').text
+    tree = ET.fromstring(data)
+
+    details = []
+    for i in range(len(tree[0])):
+        season = tree[0][i].attrib['season']
+        race = tree[0][i].find('{http://ergast.com/mrd/1.5}RaceName').text
+        name = tree[0][i].find('{http://ergast.com/mrd/1.5}ResultsList').find('{http://ergast.com/mrd/1.5}Result').find('{http://ergast.com/mrd/1.5}Driver').find('{http://ergast.com/mrd/1.5}GivenName').text
+        last_name = tree[0][i].find('{http://ergast.com/mrd/1.5}ResultsList').find('{http://ergast.com/mrd/1.5}Result').find('{http://ergast.com/mrd/1.5}Driver').find('{http://ergast.com/mrd/1.5}FamilyName').text
+        constructor = tree[0][i].find('{http://ergast.com/mrd/1.5}ResultsList').find('{http://ergast.com/mrd/1.5}Result').find('{http://ergast.com/mrd/1.5}Constructor').find('{http://ergast.com/mrd/1.5}Name').text
+        details.append([season, race, (name + ' ' + last_name), constructor])
+
+    hist = pd.DataFrame(details, columns=['Season', 'Race Name', 'Driver', 'Constructor'])
+
+    last_year = hist.iloc[[-1]]['Season'].values[0]
+
+    data = requests.request("GET", 'http://ergast.com/api/f1/' + last_year + f'/circuits/{circuit_id}/results?limit=1000').text
+    tree = ET.fromstring(data)
+
+    details = []
+    for i in tree[0][0].find('{http://ergast.com/mrd/1.5}ResultsList').findall('{http://ergast.com/mrd/1.5}Result'):
+        position = i.attrib['position']
+        number = i.attrib['number']
+        name = i.find('{http://ergast.com/mrd/1.5}Driver').find('{http://ergast.com/mrd/1.5}GivenName').text
+        last_name = i.find('{http://ergast.com/mrd/1.5}Driver').find('{http://ergast.com/mrd/1.5}FamilyName').text
+        constructor = i.find('{http://ergast.com/mrd/1.5}Constructor').find('{http://ergast.com/mrd/1.5}Name').text
+        emoji = '🥇' if position == '1' else '🥈' if position == '2' else '🥉' if position == '3' else ' '
+        details.append([position, number, (name + ' ' + last_name), constructor, emoji])
+
+    last_year_details = pd.DataFrame(details, columns=['Position', 'Number', 'Driver', 'Constructor', 'Medal'])
+
+    return [hist, last_year, last_year_details]
 
 instance = f1()
